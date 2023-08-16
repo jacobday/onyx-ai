a1411ea0f7d784099d87be9fecefaeed0331089a
Jacob Day <26262942+jacobday@users.noreply.github.com>
2023-08-16 18:32:54 -0500

    added conversation page

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -69,14 +69,14 @@ const Sidebar = () => {
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
         <Link href="/dashboard" className="flex items-center pl-3 mb-14">
-          {/* Genius Logo */}
+          {/* Onyx Logo */}
           <div className="relative w-8 h-8 mr-4">
             <Image fill alt="logo" src="/logo.png" />
           </div>
 
           {/* Heading */}
           <h1 className={cn("text-2xl font-bold", montserrat.className)}>
-            Genius
+            Onyx
           </h1>
         </Link>
 

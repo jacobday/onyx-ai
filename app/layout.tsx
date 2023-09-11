5c2cca0da07d2fd9cdaa677e3f5862c1d0b87c51
Jacob Day <26262942+jacobday@users.noreply.github.com>
2023-09-11 18:17:58 -0500

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+
 import { ModalProvider } from "@/components/modal-provider";
 import { ToasterProvider } from "@/components/toaster-provider";
+import GoogleAnalytics from "@/components/google-analytics";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +23,10 @@ export default async function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <GoogleAnalytics
+          GA_TRACKING_ID={process.env.GA_TRACKING_ID as string}
+        />
+
         <body className={inter.className}>
           <ModalProvider />
           <ToasterProvider />
--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,4 @@
 import { authMiddleware } from "@clerk/nextjs";
-import { NextRequest, NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
